461cbce8fc0976df4746f0db36c2904ae191c543
prince kabutey <princemoro20099@gmail.com>
2024-09-13 23:28:44 +0000

    Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+print(sum)
+
+
+number1 = 10
+number2 = 5
+subraction = number1 - number2
+print(subraction)
+
+number1 = 10
+number = 5
+product = number1 * number2
+print(product)
