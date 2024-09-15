a11b3c820361f50a34546857accf3ef5c492be97
prince kabutey <princemoro20099@gmail.com>
2024-09-15 22:52:02 +0000

    Convert Hours to Seconds

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,5 @@
+age = input("how old are you?: ")
+
+user_2023 = 23 in 2023
+user_2050 = 2023 - 2050
+print(user_2050)
--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,4 +1,4 @@
 hours = 2
 seconds_in_hours = 3600
-seconds = 2 * 3600 
+seconds = hours * 3600 
 print(seconds)
