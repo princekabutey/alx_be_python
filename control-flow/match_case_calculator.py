dadce01d3dfcc45e7a5d6fb14b00e4a91d45eff4
prince kabutey <princemoro20099@gmail.com>
2024-09-27 16:17:38 +0000

    Simple Calculator with Match Case

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,36 @@
+def calculator():
+    print("simple calculator")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operator = input("Choose the operation (+, -, *, /): ")
+    
+match operator:
+        case "+":
+            result = num1 + num2
+        case  "-":
+            result = num1- num2
+        case "*":
+            result = num1 * num2
+        case "/":
+            if num2 != 0:
+                resut = num1 / num2
+            else:
+                 print("cannot divide by zero")       
+
+print(f"the result is {result}")
+
+    
+     
+        
+        
+    
+
+    
+
+
