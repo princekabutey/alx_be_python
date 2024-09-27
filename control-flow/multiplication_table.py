d1ecb58ee17ade597c98813cff5202d85bfb75e5
prince kabutey <princemoro20099@gmail.com>
2024-09-27 21:19:52 +0000

    Multiplication Table Generator

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,11 @@
+
+
+    #prompt user for a number
+number = int(input("Enter a number to see its multiplication table: "))
+
+    # Generate and print the multiplication table
+  
+print(f"Multiplication Table for {number}:")
+for i in range(1, 11):
+    product = number * i
+    print(f"{number} * {i} = {product}")
