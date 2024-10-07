3295bda1dfb3a9034b47441b8096edc7db76348e
prince kabutey <princemoro20099@gmail.com>
2024-10-07 23:06:41 +0000

    Shopping List Manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -3,14 +3,12 @@ shopping_list = []
 
 def display_menu():
     """Display the shopping list menu"""
-    print(f"""\
-        "Shopping List Manager"
-        ---------------------
-        1. Add item
-        2. Remove item
-        3. View list
-        4. Exit
-    """)
+    print(f"Shopping List Manager")
+    print(f"---------------------")
+    print(f"1. Add Item")
+    print(f"2. Remove Item")
+    print(f"3. View List")
+    print(f"4. Exit")
 
 def add_item():
     """Add an item to the shopping list"""
@@ -37,22 +35,26 @@ def view_list():
         print("Your shopping list is empty.")
 
 def main():
+    shopping_list = []
     """Main function to manage the shopping list"""
     while True:
         display_menu()
         choice = input("Enter your choice (1-4): ")
         
         if choice == "1":
+            #prompt for and add an item pass
             add_item()
         elif choice == "2":
+            # prompt for and remove an item pass
             remove_item()
         elif choice == "3":
+            # Display the shopping list pass
             view_list()
         elif choice == "4":
-            print("Exiting the shopping list manager.")
+            print("Goodbye!.")
             break
         else:
-            print("Invalid choice. Please choose a valid option.")
+            print("Invalid choice. please try again.")
 
 if __name__ == "__main__":
     main()
